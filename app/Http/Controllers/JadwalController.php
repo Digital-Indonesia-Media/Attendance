4a8419e7430d85fef3a47860e534e6109aba57d6
unknown <dawwas.inha@gmail.com>
2021-11-02 11:03:07 +0700

    Penambahan Fitur Ortu

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -26,6 +26,7 @@ class JadwalController extends Controller
     	$mapels = MataPelajaran::all();
         $gurus = User::where('role', 'guru')->get();
 
+        return $datas;
     	return view('jadwal.index', compact('datas', 'tahunAjarans', 'kelass', 'mapels', 'gurus'));
     }
 
